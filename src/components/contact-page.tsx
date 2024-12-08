256082a145da89449e781f7a1b3d0cc0a57ef95c
Zachary Johnson <zachary.business@tutanota.com>
2024-12-08 14:12:57 -0500

    update iframe on contact page

--- src/components/contact-page.tsx
+++ src/components/contact-page.tsx
@@ -44,7 +44,7 @@ export function ContactPageComponent() {
           </div>
           <div className="flex items-start space-x-4">
             <MapPin className="h-5 w-5 text-gray-500 mt-1" />
-            <div className="flex flex-col">
+            <div className="flex flex-col w-full">
               <Link
                 href="https://www.google.com/maps/search/?api=1&query=3450+US+Hwy+1+S,+Saint+Augustine,+FL+32086"
                 target="_blank"
@@ -53,7 +53,7 @@ export function ContactPageComponent() {
               >
                 3450 US Hwy 1 S, Saint Augustine, FL 32086
               </Link>
-              <div className="mt-2 w-full h-64 rounded-md overflow-hidden">
+              <div className="mt-2 w-full h-64 md:h-80 lg:h-96 rounded-md overflow-hidden">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3460.5511120403247!2d-81.31808368489!3d29.846099981951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e6d9f0b1d7e5e7%3A0x1b5c5b1d1b1b1b1b!2s3450%20US-1%2C%20St%20Augustine%2C%20FL%2032086!5e0!3m2!1sen!2sus!4v1621234567890!5m2!1sen!2sus"
                   width="100%"
