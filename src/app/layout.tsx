ca1689868c6921039182b5c7f5fed15b8a497e81
Zachary Johnson <zachary.business@tutanota.com>
2024-12-17 12:21:44 -0500

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -62,6 +63,7 @@ export default function RootLayout({
         <AppHeaderComponent />
         <main>{children}</main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
