a27f7c2890ee13600acf95aae797dac3cc4b6c75
Zachary Johnson <zachary.business@tutanota.com>
2026-08-07 14:55:13 -0400

    Update clinic hours: open at 8 AM, Wed close at 2 PM (#6)

--- src/components/shared/constants.ts
+++ src/components/shared/constants.ts
@@ -1,6 +1,6 @@
 export const CLINIC_HOURS = [
-  { days: "Mon, Tue, Thu, Fri", time: "7:00 AM - 5:30 PM" },
-  { days: "Wed", time: "7:00 AM - 3:00 PM" },
+  { days: "Mon, Tue, Thu, Fri", time: "8:00 AM - 5:30 PM" },
+  { days: "Wed", time: "8:00 AM - 2:00 PM" },
   { days: "Sat, Sun", time: "Closed" },
 ];
 
